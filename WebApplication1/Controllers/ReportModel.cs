e532268cd8975d62df996dca3f63640135dda1d1
M-Piper <margaret.f.piper@gmail.com>
2024-06-18 14:16:55 -0400

    added report query and model

---
+++ WebApplication1/Controllers/ReportModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WebApplication1.Controllers
+{
+    public class ReportModel
+    {
+        public int pilotID { get; set; }
+        public string? ratingName { get; set; }
+        public DateTime? ratingDate { get; set; }
+        public string? medicalName { get; set; }
+        public string? medicalDate { get; set; }
+        public float? totalTime { get; set; }
+        public float? totalPIC { get; set; }
+        public float? totalDual { get; set; }
+        public float? timeOnType { get; set; }
+        public string? typeName { get; set; }
+        public float? totalNight { get; set; }
+        public float? nightNoInstrument { get; set; }
+        public float? totalCrossCountry { get; set; }
+        public float? totalLast30Days { get; set; }
+        public float? totalLast90Days { get; set; }
+        public float? totalLast6Months { get; set; }
+        public float? totalLast12Months { get; set; }
+        public float? totalLast24Months { get; set; }
+        public float? totalLast60Months { get; set; }
+        public float? approachesLast6Months { get; set; }
+        public float? daysSincePIC { get; set; }
+        public float? daysSinceIPC { get; set; }
+        public float? daysSinceCurrencyUpgrade { get; set; }
+
+    }
+
+}
